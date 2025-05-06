85ffa4fa9efe4dcf6e5a299559c2165fb5f66570
balatuluva <bala.tulva@gmail.com>
2025-05-06 06:23:45 +0100

    ec2 subnet id correction by index

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ALB_EC2" {
   availability_zone = element(var.azs, count.index)
   instance_type = "t2.micro"
   key_name = var.key_name
-  subnet_id = var.ALB_VPC_Private_Subnet[count.index]
+  subnet_id = aws_subnet.ALB_VPC_Private_Subnet[count.index].id
   vpc_security_group_ids = ["${aws_security_group.ALB_VPC_SG.id}"]
   associate_public_ip_address = false
   iam_instance_profile = "Delete-Later"
