57b81bf8315111a0ea8ba903ad9d5a11fb4fdb72
balatuluva <bala.tulva@gmail.com>
2025-05-06 06:07:23 +0100

    ec2 correction

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "ALB_EC2" {
-  count = 3
+  count = length(var.ALB_VPC_Private_Subnet)
   ami = "ami-0f9de6e2d2f067fca"
   availability_zone = element(var.azs, count.index)
   instance_type = "t2.micro"
   key_name = var.key_name
-  subnet_id = "${element(var.ALB_VPC_Private_Subnet, count.index)}"
+  subnet_id = element(var.ALB_VPC_Private_Subnet.id, count.index)
   vpc_security_group_ids = ["${aws_security_group.ALB_VPC_SG.id}"]
   associate_public_ip_address = false
   iam_instance_profile = "Delete-Later"
